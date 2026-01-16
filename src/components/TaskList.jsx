3642ee042a990dc899d3048abfe4a2a39ad8fd0d
farhan <farhanasfar21@gmail.com>
2026-01-16 15:22:54 +0600

    Add a loding condition before rendering task list
    Show a loading text if it takes time to fetch data

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,20 +1,36 @@
+import { useState } from "react";
 import { useFetchData } from "../hooks/useFetchData";
 
 export function TaskList() {
-    const [data] = useFetchData('https://jsonplaceholder.typicode.com/todos');
+    const [data, loading] = useFetchData('https://jsonplaceholder.typicode.com/todos');
 
+
+
+    if (loading){
+        return (
+            <h1>Loading...</h1>
+        )
+    } 
     return (
         <>
             <h1>Task List</h1>
-            <div>
+            <div className="flex flex-wrap">
                 {
+
                     data &&
                     data.map((task) => {
                         return (
-                            <div key={task.id} style={{border:"2px solid blue", padding:"10px"}}>
-                                <h2>{task.id}</h2>
-                                <p>{task.title}</p>
-                                <p>{task.completed}</p>
+                            <div key={task.id}
+                                className="flex flex-col bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs"
+                            >
+                                <h2>Task Number: {task.id}</h2>
+                                <h5 className="mb-3 flex flex-wrap text-2xl font-semibold tracking-tight text-heading leading-8">{task.title}</h5>
+                                <p>Task Status: {task.completed ? 'DONE' : 'DUE'}</p>
+
+                                <a href="#" className="inline-flex items-center text-black bg-brand box-border border hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
+                                    Task Details
+
+                                </a>
                             </div>
                         )
                     })
