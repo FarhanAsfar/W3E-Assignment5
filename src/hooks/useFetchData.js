90525b36bcd07c64ca6f435f05b43e9424bd75d3
farhan <farhanasfar21@gmail.com>
2026-01-16 15:25:10 +0600

    Add loading state

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 
 export function useFetchData(url) {
     const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         fetch(url)
             .then((res) => res.json())
-            .then((data) => setData(data));
+            .then((data) => setData(data), setLoading(false));
     }, [url])
 
-    return [data];
+    return [data, loading];
 }
