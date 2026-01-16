897651671f61ea7564efb44a0b3348b93cdaade5
farhan <farhanasfar21@gmail.com>
2026-01-16 15:52:24 +0600

    Add error state to catch error during fetch call

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -3,14 +3,22 @@ import { useEffect, useState } from "react";
 export function useFetchData(url) {
     const [data, setData] = useState();
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
        setTimeout(function (){
          fetch(url)
             .then((res) => res.json())
-            .then((data) => setData(data), setLoading(false));
+            .then((data) => {
+                setData(data);
+                setLoading(false);
+            })
+            .catch((err) => {
+                setError(err);
+                setLoading(false);
+            })
        }, 2000)
     }, [url])
 
-    return [data, loading];
+    return [data, loading, error];
 }
