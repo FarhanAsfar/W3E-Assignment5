9c42a11874b279899bcafd5a7eb962e4eac417f4
farhan <farhanasfar21@gmail.com>
2026-01-16 12:03:02 +0600

    Render TaskList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { TaskList } from './components/TaskList'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-
+      <TaskList />      
     </>
   )
 }
