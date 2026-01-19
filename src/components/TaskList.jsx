ef0942bea839567f5cafa25063429b8082b626f6
farhan <farhanasfar21@gmail.com>
2026-01-19 10:57:26 +0600

    Add a button to show the DONE/DUE status

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -37,11 +37,17 @@ export function TaskList() {
                     filteredData?.map((task) => {
                         return (
                             <div key={task.id}
-                                className="flex flex-col bg-neutral-primary-soft block max-w-sm p-6 border border-default rounded-base shadow-xs"
+                                className="flex flex-col bg-neutral-primary-soft  max-w-sm p-6 border border-default rounded-base shadow-xs h-full"
                             >
-                                <h2>Task Number: {task.id}</h2>
-                                <h5 className="mb-3 flex flex-wrap text-2xl font-semibold tracking-tight text-heading leading-8">{task.title}</h5>
-                                <p>Task Status: {task.completed ? 'DONE' : 'DUE'}</p>
+                                <h2 className="mb-2">Task Number: {task.id}</h2>
+                                <h5 className="mb-3 text-2xl font-semibold tracking-tight text-heading leading-8">{task.title}</h5>
+                                
+                                <div className="flex justify-between items-center mb-4">
+                                    <p className="font-medium">Task Status:</p>
+                                    <button className="px-3 py-1 rounded-md text-sm font-semibold transition-colors">
+                                        DONE
+                                    </button>
+                                </div>
 
                                 <a href="#" className="inline-flex items-center text-black bg-brand box-border border hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-4 py-2.5 focus:outline-none">
                                     Task Details
